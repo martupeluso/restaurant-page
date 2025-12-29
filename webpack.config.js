2471d9dc61d903169b91093621e29a474e50bfa0
Martu Peluso <martupeluso@proton.me>
2025-12-29 00:37:16 -0300

    Set up Webpack

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.(png|jpg)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2)$/i,
+        type: "asset/resource",
+      }
+    ]
+  }
+}
