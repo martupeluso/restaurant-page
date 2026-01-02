d5213ebf8c16b3ab9ac8b420b66348456265663a
Martu Peluso <martupeluso@proton.me>
2026-01-02 19:50:19 -0300

    Refactor the home module's styling

--- src/home.js
+++ src/home.js
@@ -1,11 +1,7 @@
 import { div } from "./index.js";
-import HomeImage from "./assets/images/home.jpg";
 
 function loadHome(){
-  div.style.backgroundImage = `url(${HomeImage})`;
-  div.classList.add("overlay");
-  div.style.backgroundSize = "cover";
-  div.style.backgroundPosition = "center";
+  div.className = "home";
 }
 
 export default loadHome;
